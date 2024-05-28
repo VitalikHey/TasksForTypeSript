a90851dd116996445cd3a33c1078c7c8af98c015
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-05-28 17:19:19 +0300

    Refactoring 1-2 tasks

--- script.ts
+++ script.ts
@@ -1,12 +1,12 @@
 // Задание 1
 
-export type Pick<T, K extends keyof T> = {
+export type Pick<T extends object, K extends keyof T> = {
     [P in K]: T[P];
 }
 
 // Задание 2
 
-export type Readonly<T> = {
+export type Readonly<T extends object> = {
     readonly [P in keyof T]: T[P]
 }
 
@@ -16,6 +16,6 @@ export type Readonly<T> = {
 // и у типа появляются новые свойства? Я сделал вот это вот, но по факту оно просто добавит к каждому типу | P и всё,
 // уверен что это не то, что нужно
 
-export type push<V, U extends V> = {
-    [P in keyof V]: [V | U]
+type push<V, U> = {
+    [P in keyof V]: [V, U]
 }
